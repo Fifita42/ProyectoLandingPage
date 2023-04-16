07125bb13ff19d0227826c2944303b99460a81ef
Fifita42 <tobiasmolina160@gmail.com>
2023-04-15 23:29:39 -0300

    responsive

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
+const btnPopup = document.querySelectorAll('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close')
+const contenido = document.querySelector('.contenido');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -13,12 +14,23 @@ loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
+btnPopup.forEach(boton=>{
+    boton.addEventListener('click', ()=> {
+        wrapper.classList.add('active-popup');
+        contenido.classList.add('active');
+    });
+}),
+
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+    contenido.classList.remove('active');
+
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(inp=>{
+        inp.value = null;
+    });
+
 });
 /*
 
