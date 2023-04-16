acf97ad079d0712b262244520a6ca231e066ae93
Fifita42 <tobiasmolina160@gmail.com>
2023-04-16 12:48:07 -0300

    icono

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     const contenido = document.querySelector('.contenido');
     
     
-    $('.login-register p a').click(()=> {
+    $('.login-register p span').click(()=> {
         $(wrapper).toggleClass('active');
     });
 
@@ -20,34 +20,4 @@ $(document).ready(function(){
         $('input').val(null);
     
     });
-    /*
-    
-    const formulario = document.querySelector("form");
-    
-    formulario.addEventListener("submit", function(event) {
-        event.preventDefault();
-    
-        const email = document.querySelector("input[type=email]").value;
-        const password = document.querySelector("input[type=password]").value;
-    
-        if (email.trim() === "") {
-            alert("Por favor ingrese su correo electrónico");
-            return;
-        }
-    
-        if (!/\S+@\S+\.\S+/.test(email)) {
-            alert("Por favor ingrese un correo electrónico válido");
-            return;
-        }
-    
-        if (password.trim() === "") {
-            alert("Por favor ingrese su contraseña");
-            return;
-        }
-    
-        alert("El formulario se envió correctamente");
-        formulario.reset();
-    }); */
-    
 });
-
