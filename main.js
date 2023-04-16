19092b67591ebf72132a7cac761dc7abfb2955db
Fifita42 <tobiasmolina160@gmail.com>
2023-04-16 00:11:06 -0300

    jquery

--- main.js
+++ main.js
@@ -1,62 +1,53 @@
-
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelectorAll('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close')
-const contenido = document.querySelector('.contenido');
-
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-});
-
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-});
-
-btnPopup.forEach(boton=>{
-    boton.addEventListener('click', ()=> {
-        wrapper.classList.add('active-popup');
-        contenido.classList.add('active');
-    });
-}),
-
-
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
-    contenido.classList.remove('active');
-
-    const inputs = document.querySelectorAll('input');
-    inputs.forEach(inp=>{
-        inp.value = null;
+$(document).ready(function(){
+    const wrapper = document.querySelector('.wrapper');
+    const btnPopup = document.querySelectorAll('.btnLogin-popup');
+    const iconClose = document.querySelector('.icon-close')
+    const contenido = document.querySelector('.contenido');
+    
+    
+    $('.login-register p a').click(()=> {
+        $(wrapper).toggleClass('active');
     });
 
+    $(btnPopup).click(()=> {
+        $(wrapper).toggleClass('active-popup');
+        $(contenido).toggleClass('active');
+    });
+    
+    $(iconClose).click(()=> {
+        $(wrapper).removeClass('active-popup');
+        $(contenido).removeClass('active');
+        $('input').val(null);
+    
+    });
+    /*
+    
+    const formulario = document.querySelector("form");
+    
+    formulario.addEventListener("submit", function(event) {
+        event.preventDefault();
+    
+        const email = document.querySelector("input[type=email]").value;
+        const password = document.querySelector("input[type=password]").value;
+    
+        if (email.trim() === "") {
+            alert("Por favor ingrese su correo electrónico");
+            return;
+        }
+    
+        if (!/\S+@\S+\.\S+/.test(email)) {
+            alert("Por favor ingrese un correo electrónico válido");
+            return;
+        }
+    
+        if (password.trim() === "") {
+            alert("Por favor ingrese su contraseña");
+            return;
+        }
+    
+        alert("El formulario se envió correctamente");
+        formulario.reset();
+    }); */
+    
 });
-/*
 
-const formulario = document.querySelector("form");
-
-formulario.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const email = document.querySelector("input[type=email]").value;
-    const password = document.querySelector("input[type=password]").value;
-
-    if (email.trim() === "") {
-        alert("Por favor ingrese su correo electrónico");
-        return;
-    }
-
-    if (!/\S+@\S+\.\S+/.test(email)) {
-        alert("Por favor ingrese un correo electrónico válido");
-        return;
-    }
-
-    if (password.trim() === "") {
-        alert("Por favor ingrese su contraseña");
-        return;
-    }
-
-    alert("El formulario se envió correctamente");
-    formulario.reset();
-}); */
