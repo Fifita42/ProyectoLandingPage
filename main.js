247c4d74f1a0924f8ec981fbf33bff62a0ab16fd
Fifita42 <tobiasmolina160@gmail.com>
2023-04-17 16:40:08 -0300

    js arregldo

--- main.js
+++ main.js
@@ -41,64 +41,37 @@ $(document).ready(function(){
     });
 
 
-const form = document.querySelector('form');
-const nameInput = document.getElementById('name');
-const phoneInput = document.getElementById('phone');
-const emailInput = document.getElementById('email');
-const messageInput = document.getElementById('message');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
+    const form = document.querySelector('.submitMessage');
 
-  const name = nameInput.value.trim();
-  const phone = phoneInput.value.trim();
-  const email = emailInput.value.trim();
-  const message = messageInput.value.trim();
-
-  if (name === '') {
-    alert('Por favor, ingrese su nombre');
-    nameInput.focus();
-    return;
-  }
-
-  if (phone === '') {
-    alert('Por favor, ingrese su teléfono');
-    phoneInput.focus();
-    return;
-  } else if (!isValidPhone(phone)) {
-    alert('Por favor, ingrese un número de teléfono válido');
-    phoneInput.focus();
-    return;
-  }
-
-  if (email === '') {
-    alert('Por favor, ingrese su correo electrónico');
-    emailInput.focus();
-    return;
-  } else if (!isValidEmail(email)) {
-    alert('Por favor, ingrese un correo electrónico válido');
-    emailInput.focus();
-    return;
-  }
-
-  if (message === '') {
-    alert('Por favor, ingrese su mensaje');
-    messageInput.focus();
-    return;
-  }
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const phone =  document.getElementById('phone').value.trim();
+        const message = document.getElementById('message').value.trim();
 
 
-  form.submit();
-});
-
-function isValidEmail(email) {
-  const re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
-
-function isValidPhone(phone) {
-  const re = /^\d{10}$/;
-  return re.test(phone);
-}
+        if (phone === '') {
+        alert('Por favor, ingrese su teléfono');
+        document.getElementById('phone').focus();
+        return;
+        } else if (!isValidPhone(phone)) {
+        alert('Por favor, ingrese un número de teléfono válido');
+        document.getElementById('phone').focus();
+        return;
+        }
+        else if (message === '') {
+        alert('Por favor, ingrese su mensaje');
+        document.getElementById('message').focus();
+        return;
+        }
+        else
+        {
+        form.submit();
+        }
+        function isValidPhone(phone) {
+        const re = /^\d{10}$/;
+        return re.test(phone);
+        }
+    });
 
 });
